74800b177ca9edff5b93d1804fe881855d1d508c
nafim96 <monforingnasir.com>
2021-06-08 17:32:48 +0600

    completed contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,35 @@
 import React from "react";
 import Navbar from "./Navbar";
-
+import { makeStyles } from "@material-ui/core/styles";
+const useStyle = makeStyles((theme) => ({
+  skills: {
+    background: "#0d1442",
+    margin: theme.spacing(1),
+    padding: theme.spacing(2),
+    fontSize: "20px",
+    fontWeight: "bold",
+    listStyle: "none",
+    color: "#ffffff",
+    textAlign: "center",
+  },
+  skill: {
+    background: "#0526",
+    margin: theme.spacing(1),
+    padding: theme.spacing(2),
+    fontSize: "20px",
+    fontWeight: "bold",
+    color: "#ffffff",
+    listStyle: "none",
+    textAlign: "center",
+  },
+  contact: {
+    color: "#ffffff",
+    margin: theme.spacing(1),
+    padding: theme.spacing(3),
+  },
+}));
 const Contact = () => {
+  const classes = useStyle();
   return (
     <>
       <Navbar />
@@ -20,7 +48,7 @@ const Contact = () => {
             }}
           >
             <h3 className="text-center">Contact Info</h3>
-            <div>
+            <div className={classes.contact}>
               <h4>Nasir Uddin</h4>
               <h5>Phone: (+880 1679-797459)</h5>
               <h5>E-mail: nasir.uddin.inc1227@gmail.com</h5>
@@ -36,8 +64,15 @@ const Contact = () => {
               borderRadius: "10px",
             }}
           >
-            <h3>Skills & Endorsement</h3>
-            <div></div>
+            <h3 style={{ textAlign: "center", padding: "3px" }}>
+              Skills & Endorsement
+            </h3>
+            <div>
+              <li className={classes.skills}>JavaScript</li>
+              <li className={classes.skill}>React</li>
+              <li className={classes.skills}>Node.js</li>
+              <li className={classes.skill}>MongoDB</li>
+            </div>
           </div>
         </div>
       </div>
